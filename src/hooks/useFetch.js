2952cb7b6e8c84d8022df0ee525afd526530f4a7
mpuchyr <matt.puchyr@gmail.com>
2020-07-10 14:48:11 -0500

    Add variables necessary for fetch to work in hooks/useFetch.js

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,7 +1,12 @@
-import React from 'react
+import React, { useState } from 'react'
+import md5 from 'md5'
+
 
 function useFetch () {
-
+    const publicKey = process.env.REACT_APP_API_KEY
+    const privateKey = process.env.REACT_APP_API_PRIVATE_KEY
+    const url = "https://gateway.marvel.com/"
+    const charsUrl = url + 'v1/public/characters'
 }
 
 export default useFetch
