e71ded6d4f252eb6000e815eeb97247d45c0e0be
mpuchyr <matt.puchyr@gmail.com>
2020-07-10 14:52:47 -0500

    Add timeStamp, hash, and fullUrl variables to hooks/useFetch.js

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -2,11 +2,15 @@ import React, { useState } from 'react'
 import md5 from 'md5'
 
 
-function useFetch () {
+function useFetch (letter) {
     const publicKey = process.env.REACT_APP_API_KEY
     const privateKey = process.env.REACT_APP_API_PRIVATE_KEY
     const url = "https://gateway.marvel.com/"
     const charsUrl = url + 'v1/public/characters'
+
+    let timeStamp = Date.now()
+    let hash = md5(timeStamp + privateKey + publicKey)
+    let fullUrl = charsUrl + `?nameStartsWith=${letter}&limit=100&ts=${timeStamp}&apikey=${publicKey}&hash=${hash}`
 }
 
 export default useFetch
