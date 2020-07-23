704b500f32bbe240404409f54360901d8e39316c
mpuchyr <matt.puchyr@gmail.com>
2020-07-23 14:06:24 -0500

    Include while loops in components/Characters.js to fetch characters until a charater with an image is found

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -5,6 +5,7 @@ import DisplayCharacter from './DisplayCharacter'
 function Characters() {
 
     const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    let badImg = "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available"
 
     let randLetterOne = alphabet[Math.floor(Math.random() * alphabet.length)]
     let randLetterTwo = alphabet[Math.floor(Math.random() * alphabet.length)]
@@ -13,7 +14,19 @@ function Characters() {
     const [charactersB] = useFetch(randLetterTwo)
 
     let charOne = charactersA[Math.floor(Math.random() * charactersA.length)]
+    if (charOne) {
+        while (charOne.thumbnail.path === badImg) {
+            charOne = charactersA[Math.floor(Math.random() * charactersA.length)]
+        }
+    }
+
+
     let charTwo = charactersB[Math.floor(Math.random() * charactersB.length)]
+    if (charTwo) {
+        while (charTwo.thumbnail.path === badImg) {
+            charTwo = charactersB[Math.floor(Math.random() * charactersB.length)]
+        }
+    }
 
     function showCharacters () {
         console.log(charactersA)
