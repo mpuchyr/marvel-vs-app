c0324c1966c2b4a0e6a915b80c7390b8a918ed4d
mpuchyr <matt.puchyr@gmail.com>
2020-07-23 13:47:44 -0500

    Change components/DisplayCharacter.js to show character's image based on props

--- src/components/DisplayCharacter.js
+++ src/components/DisplayCharacter.js
@@ -4,9 +4,11 @@ function DisplayCharacter(props) {
 
     if (props.character !== undefined) {
         
+        const charImg = props.character.thumbnail.path + '.' + props.character.thumbnail.extension
 
         return (
             <div>
+                <img src={charImg} alt="character image"/>
                 <h1>{props.character.name}</h1>
             </div>
         )
