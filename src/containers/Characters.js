1076f89d84b3bbc1b1af18b01940409b1c4e2c94
mpuchyr <matt.puchyr@gmail.com>
2020-07-23 15:27:49 -0500

    Add useState to containers/Characters.js and create function addWinningCharacter, calling it in onClick on divs wrapping DisplayCharacter

--- src/containers/Characters.js
+++ src/containers/Characters.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, {useState} from 'react';
 import useFetch from '../hooks/useFetch'
 import DisplayCharacter from '../components/DisplayCharacter'
 
 function Characters() {
+    const [winningChars, setWinningChars] = useState([])
+    
     const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
     let badImg = "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available"
 
@@ -39,12 +41,22 @@ function Characters() {
         console.log(charTwo)
     }
 
+    function addWinningCharacter (char) {
+        setWinningChars([...winningChars, char])
+        console.log(winningChars)
+    }
+
     return (
         <div>
             <button onClick={() => showCharacters()}>Characters</button>
             <button onClick={() => showSingleCharacters()}>Single Characters</button>
-            <DisplayCharacter character={charOne} />
-            <DisplayCharacter character={charTwo} />
+            <div onClick={() => addWinningCharacter(charOne)}>
+                <DisplayCharacter character={charOne} />
+            </div>
+            <div onClick={() => addWinningCharacter(charTwo)}>
+                <DisplayCharacter character={charTwo} />
+            </div>
+
         </div>
     )
 }
