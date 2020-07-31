e0ecf692c76c0baf34fd91173b037fec676ad0f0
mpuchyr <matt.puchyr@gmail.com>
2020-07-31 14:59:39 -0500

    Create components/DisplayLosingCharacters.js

---
+++ src/components/DisplayLosingCharacters.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function DisplayLosingCharacters (props) {
+    return props.characters.map(char => {
+        const charImg = char.thumbnail.path + '.' + char.thumbnail.extension
+        const charName = char.name
+        const charId = char.id
+        return (
+            <img src={charImg} alt={charName} key={charId}/>
+        )
+    })
+}
+
+export default DisplayLosingCharacters
