4a898ff79c2e98582480c1aea7d0edbc03718c0e
mpuchyr <matt.puchyr@gmail.com>
2020-08-14 16:01:25 -0500

    Remove components/LosingCharacterOverlay and change properties of losing-char-img in styling.scss

--- src/components/DisplayLosingCharacters.js
+++ src/components/DisplayLosingCharacters.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import LosingCharacterOverlay from './LosingCharacterOverlay';
+
 
 function DisplayLosingCharacters (props) {
     return props.characters.map(char => {
@@ -7,10 +7,10 @@ function DisplayLosingCharacters (props) {
         const charName = char.name
         const charId = char.id
         return (
-            <>
-                <LosingCharacterOverlay />
-                <img className="losing-char-img" src={charImg} alt={charName} key={charId}/>  
-            </>
+            <div className="losing-char">
+                <img className="losing-char-img" src={charImg} alt={charName} key={charId}/>
+                 
+            </div>
             
         )
     })
